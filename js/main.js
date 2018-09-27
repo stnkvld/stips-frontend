9a28b5335fd9b9e678f604d825414f99cec91430
Владислав Сотников <stnkvld@gmail.com>
2018-09-27 16:56:22 +0300

    Add actives page without adaptive and animation for accordions

--- js/main.js
+++ js/main.js
@@ -5,4 +5,16 @@ $(function() {
     overlay.on('click', function() {
         navToggleBtn.trigger('click');
     });
+
+    $('.accordion').on('click', function(e) {
+        if ( !$(e.target).hasClass('accordion__content') &&
+              $(e.target).parents('.accordion__content').length == 0 )
+            $(this).toggleClass('collapsed');
+    });
+
+    $('.record__modify-data-value-remove').on('click', function(e) {
+        var siblingInput = $(this).siblings('input');
+        siblingInput.val('');
+        siblingInput.trigger('focus');
+    })
 });
