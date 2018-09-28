147875cf85c809f1cda393c22dbc898209fbdc8e
Владислав Сотников <stnkvld@gmail.com>
2018-09-28 17:40:58 +0300

    Bug fixes + some js for tags and tabs + start Characteristics page

--- js/main.js
+++ js/main.js
@@ -16,5 +16,20 @@ $(function() {
         var siblingInput = $(this).siblings('input');
         siblingInput.val('');
         siblingInput.trigger('focus');
-    })
+    });
+
+    $('[data-tab-id="' + $('.active[data-toggle="tab"]').data('target') + '"]').show();
+
+    $('[data-toggle="tab"]').on('click', function() {
+        var target = $(this).data('target');
+        $('[data-toggle="tab"]').removeClass('active');
+        $('[data-role="tab-content"]').hide();
+        $('[data-tab-id="' + target + '"]').show();
+        $(this).addClass('active');
+    });
+
+    $('.tag__remove').on('click', function(e) {
+        e.stopPropagation();
+        $(this).closest('.tag').detach();
+    });
 });
